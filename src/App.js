57595cace33622009b9172edb6e66ff138e877f7
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2019-03-25 17:56:59 -0400

    yarn added axios and react json form. started to get the beginnings of the front end app working and working on getting the api call to return data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react'
+import Form from 'react-jsonschema-form'
+import axios from 'axios'
 import HelloWorld from './components/HelloWorld'
 
 class App extends Component {
+  state = {
+    animals: [],
+    name: ''
+  }
+
+  handleSearchChange = (event) => {
+    axios.get('http://localhost:3000/animals').then((response) => response.json()).then((response) => {
+      console.log(response)
+      this.setState({ animals: response })
+    })
+  }
+  submit = (form) => {
+    console.log(form)
+  }
+
   render() {
-    return <HelloWorld />
+    return (
+      <section>
+        <p>Search:</p>
+        <input value={this.state.name} onChange={this.handleSearchChange} />
+      </section>
+    )
   }
 }
 
